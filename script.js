f466140802d7a0b8a27c03ce4ef205bfe74dd3e1
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-02-01 21:05:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function generatePin() {
-  // 4 digit random number
   document.getElementById("generatePin").value = Math.round(
     1000 + Math.random() * 9000
   );
 }
 
 function removeNumbers() {
-  // One by one number remove function
   const number = document.getElementById("numberKeypad").value;
   document.getElementById("numberKeypad").value = number.substring(
     0,
     number.length - 1
   );
 }
-
-// Valid pin code  function
 
 function validPinCode() {
   const generate = document.getElementById("generatePin").value;
@@ -31,8 +27,6 @@ function validPinCode() {
     clickFunc();
   }
 }
-
-// Submission and Button Reload
 
 let count = 3;
 
